5a47f6cdcc2728dd79b5da0c2f3db89442e7c4b3
MariaShebeko <marygro89@gmail.com>
2022-04-18 11:05:09 +0200

    added route to update User's avatar

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ dotenv.config();
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const multer = require("multer");
-const path = require("path");
 
 const usersRouter = require("./routes/api/users");
 const contactsRouter = require("./routes/api/contacts");
@@ -17,24 +15,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
-const tempDir = path.join(__dirname, "temp");
-
-const multerConfig = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, tempDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-  limits: {
-    fileSize: 2048,
-  },
-});
-
-const upload = multer({
-  storage: multerConfig,
-});
+app.use(express.static("public"));
 
 app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
